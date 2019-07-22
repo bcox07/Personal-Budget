c8e74f1c39a702e9e0b6786de0327f980340963b
brian <bc06255@georgiasouthern.edu>
2019-07-22 12:51:20 -0400

    Removed resx line

--- Personal Budget/Properties/Resources.Designer.cs
+++ Personal Budget/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Personal_Budget.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap refresh_512 {
-            get {
-                object obj = ResourceManager.GetObject("refresh-512", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
